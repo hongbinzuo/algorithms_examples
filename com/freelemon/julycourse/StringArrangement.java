2f9bc02b67aaf125e0acec32ce10acd2a0165935
hongbin <zuohongbin@gmail.com>
2014-08-11 10:33:02 +0800

    add case handling for repeatable chars in a string arrangement

--- com/freelemon/julycourse/StringArrangement.java
+++ com/freelemon/julycourse/StringArrangement.java
@@ -13,8 +13,42 @@ import java.util.List;
 public class StringArrangement {
     private static String str = "12345";
 
+    private static String strWithRepeatChars = "12335";
+
     public static void main(String[] args){
-        StringArrangement.arrangement(0, str.length() - 1);
+        //StringArrangement.arrangement(0, str.length() - 1); // for "12345"
+        StringArrangement.arrangementWithRepeatChars(0, strWithRepeatChars.length()-1); // can handle the case "12335"
+    }
+
+    private static boolean isSwap(int from, int to){
+        boolean isCan = true;
+
+        for (int i = from; i < to; i++) {
+            if (strWithRepeatChars.toCharArray()[to] == strWithRepeatChars.toCharArray()[i]){
+                isCan = false;
+                break;
+            }
+        }
+
+        return isCan;
+    }
+
+    public static void arrangementWithRepeatChars(int from, int to){
+        if ( from == to ) {
+            for (int i = 0; i <= to ; i++) {
+                System.out.print(strWithRepeatChars.toCharArray()[i]);
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = from; i <= to ; i++) {
+            if (!isSwap(from, i))
+                continue;
+            swapRepeat(i, from);
+            arrangementWithRepeatChars(from+1, to);
+            swapRepeat(i, from);
+        }
     }
 
     public static void arrangement(int from, int to){
@@ -35,7 +69,7 @@ public class StringArrangement {
 
     // swap the value of index i and index j for the string str
     // it will change the value of str
-    public static void swap(int i, int j){
+    private static void swap(int i, int j){
 
         char c;
         char[] result = str.toCharArray();
@@ -45,6 +79,19 @@ public class StringArrangement {
         result[j] = c;
 
         str = String.valueOf(result);
+
+    }
+
+    private static void swapRepeat(int i, int j){
+
+        char c;
+        char[] result = strWithRepeatChars.toCharArray();
+
+        c = result[i];
+        result[i] = result[j];
+        result[j] = c;
+
+        strWithRepeatChars = String.valueOf(result);
 
     }
 
