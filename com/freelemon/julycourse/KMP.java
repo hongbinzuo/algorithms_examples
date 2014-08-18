249d02ba9dc90930d8d190ffdf4656208e2d6af2
hongbin <zuohongbin@gmail.com>
2014-08-18 14:17:10 +0800

    get next array

--- com/freelemon/julycourse/KMP.java
+++ com/freelemon/julycourse/KMP.java
@@ -7,7 +7,9 @@ public class KMP {
 
     public static void main(String[] args){
         String text = "ABCABCDABCEBACAAB";
-        int[] next = KMP.getNext(text);
+        //int[] next = KMP.getNext(text);
+        //int[] next = KMP.getNext2(text.toCharArray());
+        int[] next = KMP.getNextDrZou(text.toCharArray());
 
 
         for (int i = 0; i < text.length() ; i++) {
@@ -38,5 +40,46 @@ public class KMP {
 
         return next;
 
+    }
+
+    public static int[] getNext2(char[] p){
+        int i=0, j=-1;
+        int m = p.length+1;
+        int[] b = new int[m];
+
+        b[i]=j;
+
+        while (i<m)
+        {
+            while (j>=0 && p[i]!=p[j]) j=b[j];
+            i++; j++;
+            b[i]=j;
+        }
+
+        return b;
+    }
+
+    public static int[] getNextDrZou(char[] s){
+        if ( s == null || s.length == 0 ) return null;
+
+        int size = s.length;
+        int[] next = new int[size];
+        next[0] = -1;
+
+        int index, j;
+        for (int i = 1; i < size ; i++) {
+            j = i -1;
+            index = next[j];
+
+            while((index >=0 ) && s[index] != s[j])
+                index = next[index];
+
+            if (index<0)
+                next[i] = 0;
+            else
+                next[i] = index + 1;
+        }
+
+        return next;
     }
 }
